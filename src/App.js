f4a52a6ac01dc93020e50911228547c617e9cd75
Dtheran <dtheran1@gmail.com>
2022-06-17 13:39:43 -0500

    created component topBar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import TopBar from "./components/topbar/TopBar"
+
 
 function App() {
   return (
     <div>
       Listing App
+
+      <TopBar />
     </div>
   );
 }
---
+++ src/components/topbar/TopBar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function TopBar() {
+  return (
+    <div>This is TopBar</div>
+  )
+}
+
+export default TopBar
